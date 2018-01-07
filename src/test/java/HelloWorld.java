7b1265f0d902faa832f2bf1eff887a0f0f50c723
Zsuzsanna Pálmay <zsuzsanna.palmay@gmail.com>
2018-01-07 13:09:31 +0100

    Initial commit

---
+++ src/test/java/HelloWorld.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+/**
+ * Created by zsuzsiraf on 2018. 01. 07..
+ */
+public class HelloWorld {
+
+    @Test
+    public void helloWorld() {
+        System.out.println("Hello, World! ᕕ( ᐛ )ᕗ");
+    }
+}
